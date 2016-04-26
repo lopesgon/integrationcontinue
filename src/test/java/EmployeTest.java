25395556fde5ce7ed98c27cc5585d568b112a998
Frederic Lopes Goncalves Magalhaes <fredericlopesgoncalvesmagalhaes@MacBook-Pro-de-Frederic.local>
2016-04-26 12:24:07 +0200

    Premier test unitaire sur le montant total d'une commande d'employé

---
+++ src/test/java/EmployeTest.java
@@ -0,0 +1,40 @@
+import domaine.Capsule;
+import domaine.Commande;
+import domaine.Employe;
+import java.util.ArrayList;
+import static org.testng.Assert.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author fredericlopesgoncalvesmagalhaes
+ */
+public class EmployeTest {
+  
+  private domaine.Employe employe;
+  
+  private ArrayList<domaine.Commande> setUpCommande(){
+    ArrayList<Commande> commandes = new ArrayList<Commande>();
+    commandes.add(new Commande(employe, new Capsule(0, "caps1", 0.5), 10));
+    commandes.add(new Commande(employe, new Capsule(1, "caps2", 0.75), 10));
+    return commandes;
+  }
+  
+  @BeforeMethod
+  public void setUp() throws Exception {
+    employe = new Employe(0, "UserTest", "Test");
+    ArrayList<Commande> commandes = setUpCommande();
+    employe.setCommandes(commandes);
+  }
+  
+  // @AfterMethod
+  
+  @Test
+  public void testAddCommandes(){
+    double initTotal = employe.getTotalCommandes();
+    employe.addCommande(new Commande(employe, new Capsule(3, "caps3", 0.3), 10));
+    assertTrue(initTotal + 3 == employe.getTotalCommandes());
+  }
+  
+}
